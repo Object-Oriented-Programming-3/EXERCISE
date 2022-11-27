a121ae7f67e5d75b3c8927aeec56f4f493b5c07c
Yurim Yang <yang980329@likelion.org>
2022-11-27 16:48:37 +0900

    221127 Feat :: Drawer 생성

--- app/src/main/java/com/example/objectorientedprogramming3/MainActivity.kt
+++ app/src/main/java/com/example/objectorientedprogramming3/MainActivity.kt
@@ -2,18 +2,34 @@ package com.example.objectorientedprogramming3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.objectorientedprogramming3.databinding.ActivityMainBinding
 import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
+    lateinit var appBarConfiguration: AppBarConfiguration
     var firestore : FirebaseFirestore? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        val navController = binding.frgNav.getFragment<NavHostFragment>().navController
+        appBarConfiguration = AppBarConfiguration(
+            navController.graph,
+            binding.drawerLayout
+        )
+        setupActionBarWithNavController(navController,appBarConfiguration) //appBarConfiguration 쓸거면 추가하기 !!
+        binding.drawerNav.setupWithNavController(navController)
         setContentView(binding.root)
 
-        firestore = FirebaseFirestore.getInstance()
-        binding.fragmentContainerView
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = binding.frgNav.getFragment<NavHostFragment>().navController
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
